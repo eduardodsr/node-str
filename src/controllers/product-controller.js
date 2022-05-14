eaf39bbc9b64ec1bfaed779873163985082a30bb
Eduardo Rodrigues <eduardodsr@gmail.com>
2022-05-14 00:10:42 -0300

    Async & Await

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,115 +1,102 @@
 'use strict';
 
-const mongoose = require('mongoose'); // importar o mongoose
+// importar 
+const mongoose = require('mongoose'); 
 
-const Product = mongoose.model('Product'); // importar o Product, mongoose.model
+const Product = mongoose.model('Product'); 
 
-const ValidationContract = require('../validators/fluent-validator'); // importar o  fluent-validator.js
+const ValidationContract = require('../validators/fluent-validator'); 
 
-const repository = require('../repositories/product-repository'); // importar o product-repository
+const repository = require('../repositories/product-repository'); 
 
 // POST - Create -> Criar um recurso (req)
 
-// 2.6. Criando um Produto (√)
-exports.post = (req, res, next) => {
+// 2.6. Criando um Produto
+exports.post = async(req, res, next) => {
+    let contract = new ValidationContract();
+    contract.hasMinLen(req.body.title, 3, 'O title deve conter pelo menos 3 caracteres');
+    contract.hasMinLen(req.body.slug, 3, 'O slug deve conter pelo menos 3 caracteres');
+    contract.hasMinLen(req.body.description, 3, 'O description deve conter pelo menos 3 caracteres');
 
-    let constract = new ValidationContract();
-    constract.hasMinLen(req.body.title, 3, 'O "title" deve conter pelo menos 3 caracteres');
-    constract.hasMinLen(req.body.slug, 3, 'O "slug" deve conter pelo menos 3 caracteres');
-    constract.hasMinLen(req.body.description, 3, 'O "description" deve conter pelo menos 3 caracteres');
-
-    // Se os dados forem inváligos
-    if (!constract.isValid()) {
-        res.status(400).send(constract.errors()).end();
+    // Se os dados forem inválidos
+    if (!contract.isValid()) {
+        res.status(400).send(contract.errors()).end();
         return;
     }
 
-    repository.create(req.body)
-        .then(x => {
-            res.status(201).send({ 
-                message: 'Produto cadastrado com sucesso!'
-            });
-        })
-        .catch(err => {
-            res.status(400).send({ 
-                message: 'Falha ao cadastrar ao produto!',
-                data: err
-            });
-    });
+    try {
+        await repository.create(req.body)
+        res.status(201).send({ message: 'Produto cadastrado com sucesso!' });
+        
+    } catch (e) {
+        res.status(500).send({ message: 'Falha ao processar sua requisição' });
+    }
 }
 
 // GET - Read (Ler) - Recuperar um recurso (req) ou uma coleção de recursos
 
-// 2.7. Listando os Produtos (√)
-exports.get = (req, res, next) => {
-    repository.get()
-    .then(data => {
+// 2.7. Listando os Produtos
+exports.get = async(req, res, nex) => {
+    try {
+        var data = await repository.get();
         res.status(200).send(data);
-    })
-    .catch(err => {
-        res.status(400).send(err);
-    });
+    } catch (e) {
+        res.status(500).send({ message: 'Falha ao processar sua requisição' });
+    }
 }
 
-// 2.8. Listando um Produto pelo (√)
-exports.getBySlug = (req, res, next) => {
-    repository.getBySlug(req.params.slug)
-    .then(data => {
+
+// 2.8. Listando um Produto pelo Slug
+exports.getBySlug = async(req, res, next) => {
+    try {
+        var data = await repository.getBySlug(req.params.slug);
         res.status(200).send(data);
-    })
-    .catch(err => {
-        res.status(400).send(err);
-    });
+    } catch (e) {
+        res.status(500).send({ message: 'Falha ao processar sua requisição' });
+    }
 }
 
-// 2.9. Listando um Produto pelo Id (√)
-exports.getById = (req, res, next) => {
-    repository.getById(req.params.id)
-    .then(data => {
+// 2.9. Listando um Produto pelo Id
+exports.getById = async(req, res, next) => {
+    try {
+        var data = await repository.getById(req.params.id);
         res.status(200).send(data);
-    })
-    .catch(err => {
-        res.status(400).send(err);
-    });
+    } catch (e) {
+        res.status(500).send({ message: 'Falha ao processar sua requisição' });
+    }
 }
 
-// 2.10. Listando os Produtos de uma tag (√)
-exports.getByTag = (req, res, next) => {
-    repository.getByTag(req.params.tag)
-    .then(data => {
+// 2.10. Listando os Produtos de uma Tag
+exports.getByTag = async(req, res, next) => {
+    try {
+        const data = await repository.getByTag(req.params.tag);
         res.status(200).send(data);
-    })
-    .catch(err => {
-        res.status(400).send(err);
-    });
+    } catch (e) {
+        res.status(500).send({ message: 'Falha ao processar sua requisição' });
+    }
 }
 
 // PUT - Update -> Atualizar um recurso (req)
 
-// 2.11. Atualizando um produto (√)
-exports.put = (req, res, next) => {
-    repository.update(req.params.id, req.body)
-        .then(x => {
-            res.status(200).send({ message: 'Produto atualizado com sucesso!' });
-        })
-        .catch(err => {
-            res.status(400).send({ message: 'Falha ao atualizar produto!', data: err });
-        });
+// 2.11. Atualizando um produto
+exports.put = async(req, res, next) => {
+    try {
+        await repository.update(req.params.id, req.body);
+        res.status(200).send({ message: 'Produto atualizado com sucesso!' });
+    } catch (e) {
+        res.status(500).send({ message: 'Falha ao processar sua requisição' });
+    }
 }
+
 
 // DELETE - Delete -> Excluir um recurso (req)
 
-// 2.12. Excluindo um produto (√)
-exports.delete = (req, res, next) => {
-    repository.delete(req.body.id)
-        .then(x => {
-            res.status(200).send({ 
-                message: 'Produto removido com sucesso!' 
-            });
-        }).catch(err => {
-            res.status(400).send({ 
-                message: 'Falha ao remover produto!', 
-                data: err 
-            });
-        });
+// 2.12. Excluindo um produto
+exports.delete = async(req, res, next) => {
+    try {
+        await repository.delete(req.body.id)
+        res.status(200).send({ message: 'Produto removido com sucesso!' });
+    } catch (e) {
+        res.status(500).send({ message: 'Falha ao processar sua requisição' });
+    }
 }
--- src/repositories/product-repository.js
+++ src/repositories/product-repository.js
@@ -1,96 +1,46 @@
 'use strict';
+
 const mongoose = require('mongoose');
 
-const Product = mongoose.model('Product'); // importar o Model, ele faz o acesso aos dados
+const Product = mongoose.model('Product'); 
 
 // GET - Read (Ler) - Recuperar um recurso (req) ou uma coleção de recursos
 
-// 2.7. Listando os Produtos (√)
-exports.get = () => {
-    return Product.find({ active: true }, 'title price slug');
+// 2.7. Listando os Produtos
+exports.get = async() => {
+    const res = await Product.find({ active: true }, 'title price slug');
+    return res;
 }
 
-// 2.8. Listando um Produto pelo slug (√)
-exports.getBySlug = (slug) => {
-    return Product.findOne({ slug: slug, active: true }, 'title description price slug tags');
+// 2.8. Listando um Produto pelo slug
+exports.getBySlug = async(slug) => {
+    const res = await Product.findOne({ slug: slug, active: true }, 'title description price slug tags');
+    return res;
 }
 
-// 2.9. Listando um Produto pelo (√)
-exports.getById = (id) => {
-    return Product.findById(id);
+// 2.9. Listando um Produto pelo Id
+exports.getById = async(id) => {
+    const res = await Product.findById(id);
+    return res;
 }
 
-// 2.10. Listando os Produtos de uma tag (√)
-exports.getByTag = (tag) => {
-    return Product.find({ tags: tag, active: true }, 'title description price slug tags')
+// 2.10. Listando os Produtos de uma tag
+exports.getByTag = async(tag) => {
+    const res = await Product.find({ tags: tag, active: true }, 'title description price slug tags');
+    return res;
 }
 
 // POST - Create -> Criar um recurso (req)
 
-// 2.6. Criando um Produto (√)
-exports.create = (data) => {
-    var product = new Product(data); // criar instancia do product
-    return product.save();
-}
-
-// PUT - Update -> Atualizar um recurso (req)
-
-// 2.11. Atualizando um produto (√)
-exports.update = (id, data) => {
-    return Product
-        .findByIdAndUpdate(id, {
-            $set: {
-                title: data.title,
-                description: data.description,
-                price: data.price,
-                slug: data.slug
-            }
-        });
-}
-
-// 2.12. Excluindo um produto (√)
-exports.delete = (id) => {
-    return Product
-        .findOneAndRemove(req.body.id);
-}
-
-/*
-exports.get = async() => {
-    const res = await Product.find({
-        active: true
-    }, 'title price slug');
-    return res;
-}
-
-exports.getBySlug = async(slug) => {
-    const res = await Product
-        .findOne({
-            slug: slug,
-            active: true
-        }, 'title description price slug tags');
-    return res;
-}
-
-exports.getById = async(id) => {
-    const res = await Product
-        .findById(id);
-    return res;
-}
-
-exports.getByTag = async(tag) => {
-    const res = Product
-        .find({
-            tags: tag,
-            active: true
-        }, 'title description price slug tags');
-    return res;
-}
-
+// 2.6. Criando um Produto
 exports.create = async(data) => {
     var product = new Product(data);
     await product.save();
 }
 
+// PUT - Update -> Atualizar um recurso (req)
+
+// 2.11. Atualizando um produto
 exports.update = async(id, data) => {
     await Product
         .findByIdAndUpdate(id, {
@@ -103,8 +53,8 @@ exports.update = async(id, data) => {
         });
 }
 
+// 2.12. Excluindo um produto
 exports.delete = async(id) => {
     await Product
         .findOneAndRemove(id);
 }
-*/
